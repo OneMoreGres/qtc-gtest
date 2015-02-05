2af8e2c11e50d456041d214be6000a8770206c98
Gres <gres@gres.biz>
2015-02-05 22:43:56 +0300

    Updated translation.

--- translation/QtcGtest_ru.ts
+++ translation/QtcGtest_ru.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>PaneWidget</name>
     <message>
@@ -12,12 +12,31 @@
 <context>
     <name>QtcGtest::Internal::OutputPane</name>
     <message>
-        <location filename="../src/OutputPane.cpp" line="52"/>
+        <location filename="../src/OutputPane.cpp" line="31"/>
+        <source>Auto popup pane</source>
+        <translation>Авто показ панели</translation>
+    </message>
+    <message>
+        <location filename="../src/OutputPane.cpp" line="36"/>
+        <source>Show passed tests</source>
+        <translation>Показывать успешные тесты</translation>
+    </message>
+    <message>
+        <location filename="../src/OutputPane.cpp" line="71"/>
         <source>Google Test</source>
         <translation>Google Test</translation>
     </message>
     <message>
-        <location filename="../src/OutputPane.cpp" line="171"/>
+        <location filename="../src/OutputPane.cpp" line="192"/>
+        <source>Total: passed %1 of %2 (%3 ms).</source>
+        <translation>Всего: пройдено %1 из %2 (%3 мсек.).</translation>
+    </message>
+    <message>
+        <location filename="../src/OutputPane.cpp" line="196"/>
+        <source>Disabled tests: %1.</source>
+        <translation>Отключенных тестов: %1.</translation>
+    </message>
+    <message>
         <source>Total: passed %1 of %2 (%3 ms)</source>
         <translation>Всего: пройдено %1 из %2 (%3 мсек.)</translation>
     </message>
@@ -25,37 +44,37 @@
 <context>
     <name>QtcGtest::Internal::QtcGtestPlugin</name>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="90"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="96"/>
         <source>Check project</source>
         <translation>Проверить проект</translation>
     </message>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="94"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="100"/>
         <source>Alt+T,Alt+A</source>
         <translation>Alt+T,Alt+A</translation>
     </message>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="97"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="103"/>
         <source>Check current</source>
         <translation>Проверить текущий документ</translation>
     </message>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="101"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="107"/>
         <source>Alt+T,Alt+C</source>
         <translation>Alt+T,Alt+C</translation>
     </message>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="104"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="110"/>
         <source>Check changed</source>
         <translation>Проверить изменившиеся файлы</translation>
     </message>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="108"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="114"/>
         <source>Alt+T,Alt+T</source>
         <translation>Alt+T,Alt+T</translation>
     </message>
     <message>
-        <location filename="../src/QtcGtestPlugin.cpp" line="112"/>
+        <location filename="../src/QtcGtestPlugin.cpp" line="118"/>
         <source>Google Test</source>
         <translation>Google Test</translation>
     </message>
@@ -63,32 +82,32 @@
 <context>
     <name>QtcGtest::Internal::TestModel</name>
     <message>
-        <location filename="../src/TestModel.cpp" line="14"/>
-        <location filename="../src/TestModel.cpp" line="20"/>
+        <location filename="../src/TestModel.cpp" line="15"/>
+        <location filename="../src/TestModel.cpp" line="21"/>
         <source>Name</source>
         <translation>Название</translation>
     </message>
     <message>
-        <location filename="../src/TestModel.cpp" line="15"/>
-        <location filename="../src/TestModel.cpp" line="21"/>
+        <location filename="../src/TestModel.cpp" line="16"/>
+        <location filename="../src/TestModel.cpp" line="22"/>
         <source>Passed</source>
         <translation>Пройдено</translation>
     </message>
     <message>
-        <location filename="../src/TestModel.cpp" line="16"/>
-        <location filename="../src/TestModel.cpp" line="22"/>
+        <location filename="../src/TestModel.cpp" line="17"/>
+        <location filename="../src/TestModel.cpp" line="23"/>
         <source>Failed</source>
         <translation>Провалено</translation>
     </message>
     <message>
-        <location filename="../src/TestModel.cpp" line="17"/>
-        <location filename="../src/TestModel.cpp" line="23"/>
+        <location filename="../src/TestModel.cpp" line="18"/>
+        <location filename="../src/TestModel.cpp" line="24"/>
         <source>Time</source>
         <translation>Время</translation>
     </message>
     <message>
-        <location filename="../src/TestModel.cpp" line="18"/>
-        <location filename="../src/TestModel.cpp" line="24"/>
+        <location filename="../src/TestModel.cpp" line="19"/>
+        <location filename="../src/TestModel.cpp" line="25"/>
         <source>Type</source>
         <translation>Тип</translation>
     </message>
